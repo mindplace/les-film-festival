3eee4839e18a8fffac94c297d074dc10d7f76c5f
John Colella & Katherine Broner & Esther Leytush <pair+jmcolella.katherinebroner.mindplace@devbootcamp.com>
2016-05-25 15:59:28 -0400

    complete MVP for category index and show, including views and controller; add some logic to movie model for number of reviews and average rating

--- film-festival-rails-app/app/models/movie.rb
+++ film-festival-rails-app/app/models/movie.rb
@@ -5,4 +5,17 @@ class Movie < ActiveRecord::Base
   belongs_to :category
   has_many :reviews
   has_many :comments, through: :reviews
+
+
+  def number_of_reviews
+    self.reviews.length
+  end
+
+  def average_rating
+    if self.number_of_reviews >= 1
+      ((self.reviews.reduce(0) {|sum, review| sum + review.stars}) / (self.number_of_reviews)).round(1)
+    else
+      return "No ratings yet"
+    end
+  end
 end
