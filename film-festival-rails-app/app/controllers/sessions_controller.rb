87ac547b4eeac8d0bfc5e639421a1662bcf88d63
Esther Leytush <eleytush@gmail.com>
2016-05-25 15:49:52 -0400

    add sessions controller, view, shared errors

---
+++ film-festival-rails-app/app/controllers/sessions_controller.rb
@@ -0,0 +1,36 @@
+class SessionsController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.find_by(login_username)
+    if @user && @user.authenticate(login_password[:password])
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      if @user
+        @user.errors[:password].push("incorrect")
+      else
+        @user = User.new
+        @user.errors[:username].push("not found")
+      end
+      render 'new'
+    end
+  end
+
+  def destroy
+    session.clear
+    redirect_to root_path
+  end
+
+  private
+
+  def login_username
+    params.require(:login).permit(:username)
+  end
+
+  def login_password
+    params.require(:login).permit(:password)
+  end
+end
