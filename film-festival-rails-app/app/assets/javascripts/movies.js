11ddee38e83fce527fe28b9a6687c3eb1b469576
jmcolella <colella.john@gmail.com>
2016-05-25 21:34:41 -0400

    change profile link on layout to be user's username; add debugger to javascript file

--- film-festival-rails-app/app/assets/javascripts/movies.js
+++ film-festival-rails-app/app/assets/javascripts/movies.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
 
   $("#add-new-review").on("click", "a", function(e) {
     e.preventDefault();
+    debugger;
 
 
 
--- film-festival-rails-app/app/controllers/comments_controller.rb
+++ film-festival-rails-app/app/controllers/comments_controller.rb
@@ -8,7 +8,6 @@ class CommentsController < ApplicationController
     @comment = Comment.new(comment_params)
     @comment.user = current_user
     @comment.review = Review.find_by(id: params[:review_id])
-    # I need to figure out how I'm going to get the review information so I can assign the comment
 
     if @comment.save
       redirect_to movie_path(@comment.review.movie)
