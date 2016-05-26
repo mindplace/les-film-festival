1bf94da4efa33f07f2fdd8d1451bf0966bbb813b
jmcolella <colella.john@gmail.com>
2016-05-25 20:33:51 -0400

    think about how to incorporate review information when creating a comment, update error partials for comment and review forms

--- film-festival-rails-app/app/controllers/comments_controller.rb
+++ film-festival-rails-app/app/controllers/comments_controller.rb
@@ -7,13 +7,20 @@ class CommentsController < ApplicationController
   def create
     @comment = Comment.new(comment_params)
     @comment.user = current_user
-    @comment.review =
+    # @comment.review =
+    binding.pry
     # I need to figure out how I'm going to get the review information so I can assign the comment
+
+    if @comment.save
+      redirect_to movie_path(@comment.review.movie)
+    else
+      render 'new'
+    end
 
   end
 
   def comment_params
-    self.require(:comment).permit(:body)
+    params.require(:comment).permit(:body)
   end
 
 end
