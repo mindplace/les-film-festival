9961e6b20db2d3cbfc9560d36fc6e32fa056d3de
Esther Leytush <eleytush@gmail.com>
2016-05-26 09:57:32 -0400

    fix bug, refactor with HTML tags

--- film-festival-rails-app/app/models/category.rb
+++ film-festival-rails-app/app/models/category.rb
@@ -4,7 +4,7 @@ class Category < ActiveRecord::Base
   has_many :movies
 
   def five_most_recent_reviews
-    all_reviews = movies.map{|movie| movie.reviews.flatten
+    all_reviews = movies.map{|movie| movie.reviews}.flatten
     all_reviews.sort_by{|review| review.updated_at}.reverse[0..4]
   end
 
