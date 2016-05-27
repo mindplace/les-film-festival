01015a7f84f5309b9d4bd4ee44b0abbbe89f5217
Esther Leytush <eleytush@gmail.com>
2016-05-26 22:11:18 -0400

    add tabs, minor stuff

--- film-festival-rails-app/app/assets/javascripts/reviews.js
+++ film-festival-rails-app/app/assets/javascripts/reviews.js
@@ -1,13 +1,19 @@
 $(document).ready(function(){
  $("#category_reviews").hide();
+
  $("#reviews_category_link").on("click", function(e){
-  e.preventDefault();
-  $("#reviews_all_list").hide();
-  $("#category_reviews").show();
- });
-$("#reviews_all_link").on("click", function(e){
-  e.preventDefault();
-  $("#category_reviews").hide();
-  $("#reviews_all_list").show();
-});
+    e.preventDefault();
+    $("#reviews_all_list").hide();
+    $("#reviews_all_link").parent().removeClass("active");
+    $("#reviews_category_link").parent().addClass("active");
+    $("#category_reviews").show();
+   });
+
+  $("#reviews_all_link").on("click", function(e){
+    e.preventDefault();
+    $("#category_reviews").hide();
+    $("#reviews_all_link").parent().addClass("active");
+    $("#reviews_category_link").parent().removeClass("active");
+    $("#reviews_all_list").show();
+  });
 });
