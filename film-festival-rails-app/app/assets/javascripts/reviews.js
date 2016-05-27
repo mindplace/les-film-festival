b024958c2459a447760976d38332b431db24a181
Esther Leytush <eleytush@gmail.com>
2016-05-27 00:10:40 -0400

    more tabbing for recent reviews

--- film-festival-rails-app/app/assets/javascripts/reviews.js
+++ film-festival-rails-app/app/assets/javascripts/reviews.js
@@ -1,19 +1,31 @@
 $(document).ready(function(){
- $("#category_reviews").hide();
+ $(".category").hide();
+ $("#reviews-category").show();
 
- $("#reviews_category_link").on("click", function(e){
-    e.preventDefault();
-    $("#reviews_all_list").hide();
-    $("#reviews_all_link").parent().removeClass("active");
-    $("#reviews_category_link").parent().addClass("active");
-    $("#category_reviews").show();
-   });
+ // $("#reviews_category_link").on("click", function(e){
+ //    e.preventDefault();
+ //    $("#reviews_all_list").hide();
+ //    $("#reviews_all_link").parent().removeClass("active");
+ //    $("#reviews_category_link").parent().addClass("active");
+ //    $("#category_reviews").show();
+ //   });
+ //
+ //  $("#reviews_all_link").on("click", function(e){
+ //    e.preventDefault();
+ //    $("#category_reviews").hide();
+ //    $("#reviews_all_link").parent().addClass("active");
+ //    $("#reviews_category_link").parent().removeClass("active");
+ //    $("#reviews_all_list").show();
+ //  });
 
-  $("#reviews_all_link").on("click", function(e){
+  $(".padded-nav").on("click", "a", function(e) {
     e.preventDefault();
-    $("#category_reviews").hide();
-    $("#reviews_all_link").parent().addClass("active");
-    $("#reviews_category_link").parent().removeClass("active");
-    $("#reviews_all_list").show();
-  });
+    var tab = $(this).attr("id");
+
+    $(this).parent().parent().children().removeClass("active");
+    $(this).parent().addClass("active");
+
+    $(".category").hide();
+    $("#" + tab + "-category").show();
+  })
 });
