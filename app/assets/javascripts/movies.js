1d5ded6d0a1cc7c084edc7a68242fc1d5a1ee637
jmcolella <colella.john@gmail.com>
2016-06-17 12:52:50 -0400

    add comment ajax and remove log and cache from git commits

--- app/assets/javascripts/movies.js
+++ app/assets/javascripts/movies.js
@@ -56,7 +56,6 @@ $(document).ready(function() {
   $("#list-of-judge-reviews").on("click", ".delete-link", function(e) {
     e.preventDefault();
     var url = $(e.target).attr("href");
-    // var type = $(e.target).attr("data-method");
 
     $.ajax({
       url: url,
@@ -66,4 +65,77 @@ $(document).ready(function() {
     }.bind(this));
   });
 
+  // reveal comment form for users
+  $("#list-of-user-reviews").on("click", ".new-comment-link", function(e) {
+    e.preventDefault();
+    var url = $(e.target).attr("href");
+
+    $.ajax({
+      url: url,
+      type: "GET"
+    }).done(function(response){
+      $(this).parent().append(response);
+      $(this).hide();
+    }.bind(this));
+  });
+
+  // post comment to user section
+  $("#list-of-user-reviews").on("submit", "#comment_form", function(e) {
+    e.preventDefault();
+    var url = e.target.action;
+    var type = e.target.method;
+    var data = $(e.target).serialize();
+
+    $.ajax({
+      url: url,
+      type: type,
+      data: data
+    }).done(function(response) {
+      if ($(this).parent().siblings(".comment-list").find(".no-comments").length == 0) {
+        $(this).parent().siblings(".comment-list").append(response);
+      } else {
+        $(this).parent().siblings(".comment-list").find("p").hide();
+        $(this).parent().siblings(".comment-list").append(response);
+      };
+      $(this).siblings().show();
+      $(this).remove();
+    }.bind(this));
+  });
+
+   // reveal comment form for judge section
+  $("#list-of-judge-reviews").on("click", ".new-comment-link", function(e) {
+    e.preventDefault();
+    var url = $(e.target).attr("href");
+
+    $.ajax({
+      url: url,
+      type: "GET"
+    }).done(function(response){
+      $(this).parent().append(response);
+      $(this).hide();
+    }.bind(this));
+  });
+
+  // post comment to judge section
+  $("#list-of-judge-reviews").on("submit", "#comment_form", function(e) {
+    e.preventDefault();
+    var url = e.target.action;
+    var type = e.target.method;
+    var data = $(e.target).serialize();
+
+    $.ajax({
+      url: url,
+      type: type,
+      data: data
+    }).done(function(response) {
+      if ($(this).parent().siblings(".comment-list").find(".no-comments").length == 0) {
+        $(this).parent().siblings(".comment-list").append(response);
+      } else {
+        $(this).parent().siblings(".comment-list").find(".no-comments").hide();
+        $(this).parent().siblings(".comment-list").append(response);
+      };
+      $(this).siblings().show();
+      $(this).remove();
+    }.bind(this));
+  });
 });
